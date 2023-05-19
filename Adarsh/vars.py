8556fea50f43fbee316244a00d73f4fc720877f1
Aayush Sharma <bhaskarsharma64047@gmail.com>
2023-05-19 13:18:10 +0545

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,7 +11,7 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID','17954446'))
     API_HASH = str(getenv('API_HASH','b9d62d60af7542ad41274a47a5c9f369'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN','5959633763:AAES9rWaQDsZEBT4ACWzxGqubxwQKjCMA-0'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN','6211197496:AAHoh-O4x28K-sEVv8ZTg9xhYZ6zabrnnQE'))
     name = str(getenv('SESSION_NAME', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
@@ -25,7 +25,7 @@ class Var(object):
     OWNER_USERNAME = str(getenv('OWNER_USERNAME','sigma_male_007'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME','file-2-link-pyro-max'))
+        APP_NAME = str(getenv('APP_NAME','file_2_link_pyro'))
     
     else:
         ON_HEROKU = False
